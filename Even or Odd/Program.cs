91199f21db48c77391b257db7d5d658cfd9932b8
Blooode <09vlad00@gmail.com>
2023-07-07 01:26:11 +0300

    Even or Odd Added

---
+++ Even or Odd/Program.cs
@@ -0,0 +1,17 @@
+﻿// DESCRIPTION:
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+namespace Even_or_Odd
+{
+    internal class Program
+    {
+        static string EvenOrOdd(int number) => number % 2 == 0 ? "Even" : "Odd";
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"The number {number} is {EvenOrOdd(number)}");
+        }
+    }
+}
