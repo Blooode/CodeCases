42d14a1424f6240c31c1b4d34ab5edeaecc374da
Blooode <09vlad00@gmail.com>
2023-07-07 09:04:33 +0300

    Sum of positive added

---
+++ Sum of positive/Program.cs
@@ -0,0 +1,27 @@
+﻿// DESCRIPTION:
+// You get an array of numbers, return the sum of all of the positives ones.
+// Note: if there is nothing to sum, the sum is default to 0.
+
+using System.Threading.Channels;
+
+namespace Sum_of_positive
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int[] numbers = new int[10];
+            int sum = 0;
+
+            foreach (int num in numbers)
+            {
+                numbers[num] = random.Next(-100, 100);
+                Console.Write($"{numbers[num]}, ");
+
+                if (numbers[num] > 0) sum += numbers[num];
+            }
+            Console.WriteLine($"Sum is {sum}");
+        }
+    }
+}
