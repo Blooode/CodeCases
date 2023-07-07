12b3afa171e346dc27bd7be93546785d204a87ab
Blooode <09vlad00@gmail.com>
2023-07-08 01:11:41 +0300

    Updated for chosen number

--- Multiples of 3 or 5/Program.cs
+++ Multiples of 3 or 5/Program.cs
@@ -1,5 +1,5 @@
 ﻿// DESCRIPTION:
-// Find the sum of all the multiples of 3 or 5 below 1000
+// Find the sum of all the multiples of 3 or 5 below chosen number
 
 namespace Multiples_of_3_or_5
 {
@@ -8,12 +8,14 @@ namespace Multiples_of_3_or_5
         static void Main(string[] args)
         {
             int sum = 0;
+            Console.Write("Enter the number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
 
-            for (int i = 1; i < 1000; i++)
+            for (int i = 1; i < number; i++)
             {
                 if (i % 3 == 0 || i % 5 == 0) sum += i;
             }
-            Console.WriteLine(sum);
+            Console.WriteLine($"Sum is {sum}");
         }
     }
 }
