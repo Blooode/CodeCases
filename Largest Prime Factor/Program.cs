4e98079ad74e5aa23822f756928f78e1bb86c463
Blooode <09vlad00@gmail.com>
2023-07-08 01:29:39 +0300

    Largest Prime Factor added

---
+++ Largest Prime Factor/Program.cs
@@ -0,0 +1,27 @@
+﻿// DESCRIPTION
+// The largest prime factor of chosen number
+
+namespace Largest_Prime_Factor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number: ");
+            long number = Convert.ToInt64(Console.ReadLine());
+            long primeFactor = 2;
+            Console.WriteLine("Prime factors: ");
+
+            while (number != 1)
+            {
+                if (number % primeFactor == 0)
+                {
+                    number /= primeFactor;
+                    Console.Write($"{primeFactor}, ");
+                }
+                else primeFactor++;
+            }
+            Console.WriteLine($"the largest prime factor is {primeFactor}");
+        }
+    }
+}
