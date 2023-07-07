93717c27fe336d3ece6bd1d43fa5517ffb834e6e
Blooode <09vlad00@gmail.com>
2023-07-08 01:04:17 +0300

    Multiples of 3 or 5 added

---
+++ Multiples of 3 or 5/Program.cs
@@ -0,0 +1,19 @@
+﻿// DESCRIPTION:
+// Find the sum of all the multiples of 3 or 5 below 1000
+
+namespace Multiples_of_3_or_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+
+            for (int i = 1; i < 1000; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0) sum += i;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
